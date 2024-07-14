03b98318cc3dd80eda7b6592ddeaa1e9c6c7e633
shilong.zhang <shilong.zhang2@zhenai.com>
2024-07-14 12:49:53 +0800

    gen(13/57)

--- FinQwen/opt/experiment/generate_sql_test_question.py
+++ FinQwen/opt/experiment/generate_sql_test_question.py
@@ -8,105 +8,37 @@ from dataclasses import dataclass
 from random import choice
 from typing import Callable
 
-import pandas as pd
-
 from ..tools.config import Config
 from ..tools.utils import File
 from ..tools.utils import String
 
+# TODO 待整合
+#   Generator加方法，一键测试同簇的所有问题（如果不成功，将问题打印出来，不报错）
 db = Config.get_database()
-
-# TODO 先不集成到Config/Database中，再观察一下
 db_metadata = File.json_load(Config.DATABASE_METADATA_PATH)
-column_dfs = {table: pd.DataFrame(v["字段信息"]) for table, v in db_metadata.items()}
+distinct_values_dct = {
+    table: {
+        record["字段名"]: record["唯一值抽样"]
+        for record in v["字段信息"]
+    }
+    for table, v in db_metadata.items()
+}
 
 
-def get_distinct_values(table, column):
-    return column_dfs[table].query(f"字段名 == '{column}'")["唯一值抽样"].iloc[0]
+def choice_from_column(table, column):
+    return choice(distinct_values_dct[table][column])
 
 
-def random(table, column):
-    return choice(get_distinct_values(table, column))
+def choice_from_dict(dct):
+    return choice(list(dct.keys()))
 
 
-units = ["个人", "机构"]
-standards = ["中信", "申万"]
 years = [2019, 2020, 2021]
-
-# [基金规模变动表.截止日期]的月日只有4种组合
-month_to_monthday = {
-    "03": "03-31",
-    "06": "06-30",
-    "09": "09-30",
-    "12": "12-31"
-}
-months = list(month_to_monthday.keys())
-monthdays = list(month_to_monthday.values())
-
-season_to_start_end_v1 = {
-    "Q1": ("0101", "0331"),
-    "Q2": ("0401", "0630"),
-    "Q3": ("0701", "0930"),
-    "Q4": ("1001", "1231")
-}
-seasons_v1 = list(season_to_start_end_v1.keys())
-
-season_to_start_end_v2 = {
-    "一季度": ("0101", "0331"),
-    "二季度": ("0401", "0630"),
-    "三季度": ("0701", "0930"),
-    "四季度": ("1001", "1231")
-}
-seasons_v2 = list(season_to_start_end_v2.keys())
-
-compare_to_sign_v1 = {
-    "高于": ">",
-    "低于": "<"
-}
-compares_v1 = list(compare_to_sign_v1.keys())
-
-compare_to_sign_v2 = {
-    "正": ">",
-    "负": "<"
-}
-compares_v2 = list(compare_to_sign_v2.keys())
-
-compare_to_sign_v3 = {
-    "大": ">",
-    "小": "<"
-}
-compares_v3 = list(compare_to_sign_v3.keys())
-
-compare_to_sign_v4 = {
-    "上涨": ">",
-    "下跌": "<"
-}
-compares_v4 = list(compare_to_sign_v4.keys())
-
+standards = ["中信", "申万"]
 stock_to_table = {
     "A股": "A股票日行情表",
     "港股": "港股票日行情表"
 }
-stocks = list(stock_to_table.keys())
-
-numzh_to_num = {
-    "一": 1,
-    "二": 2,
-    "三": 3,
-    "四": 4,
-    "五": 5,
-    "六": 6,
-    "七": 7,
-    "八": 8,
-    "九": 9
-}
-numzhs = list(numzh_to_num.keys())
-
-target_to_column = {
-    "成交量": "成交量(股)",
-    "成交金额": "成交金额(元)"
-}
-targets = list(target_to_column.keys())
 
 # 上面是全局环境
 # =====================================================================================================
@@ -160,6 +92,34 @@ class Decorator:
 
 # =====================================================================================================
 # 之前的generator先放到sql_generator.py
+"""
+基金基本信息：
+    管理费率、托管费率：取值形如 '1.2%', 经测试可以直接比较（如 管理费率 < '0.8%' ）
+
+基金股票持仓明细：
+    股票代码：既可能在 A股票日行情表、也可能在 港股票日行情表 上！
+    持仓日期：将初步分析，部分 报告类型 = '季报'、'年报(含半年报)' 的基金虽然可能有不在季末的报告，但季末一定有报告。因此取条件时直接以季末为准
+
+基金可转债持仓明细：
+    对应股票代码：只有A股，没有港股
+    持仓日期：将初步分析，部分 报告类型 = '季报' 的基金虽然可能有不在季末的报告，但季末一定有报告。因此取条件时直接以季末为准
+
+A股票日行情表：
+    股票代码：与 港股票日行情表 的 股票代码 没有重叠
+
+港股票日行情表：
+    股票代码：与 A股票日行情表 的 股票代码 没有重叠
+
+A股公司行业划分表：
+    股票代码：只有A股，没有港股
+    行业划分标准=中信/申万行业分类 下，有部分一级行业名称（例如 汽车 计算机）会重叠
+
+基金规模变动表：
+    截止日期：月日的组合只有4种可能（季末），连同年一共只有12种取值
+
+基金份额持有人结构：
+    个人/机构投资者持有的基金份额占总份额比例：是[0, 100]的数字
+"""
 
 
 # =====================================================================================================
@@ -181,9 +141,11 @@ def gen():
 gen()
 
 question = ""
-gen.query(question)
 
-sql1 = """
+for x in gen.query(question):
+    print(x, "\n\n")
+
+sql = """
 SELECT
 FROM
 WHERE
@@ -191,28 +153,14 @@ AND
 AND
 LIMIT 100;
 """
-db.query(sql1)
+db.query(sql)
 
 # =====================================================================================================
 # 笔记
 
 
-# TODO 聚类0、8题意模糊，不确定需不需要考虑绝对值，先不考虑，等钉钉群回复
-"""
-港股票日行情表、A股票日行情表 的 股票代码没有重叠
-基金可转债持仓明细 的 对应股票代码 只有 A股，没有港股
-A股公司行业划分表 的 股票代码 只有 A股，没有港股
+# TODO 可能有的问题：
+#  聚类0、8题意模糊，不确定需不需要考虑绝对值，先不考虑，等钉钉群回复
+#  所有用到 基金股票持仓明细、基金可转债持仓明细 报告日期的sql，日期不对（但应该不可能）
 
-基金股票持仓明细 的 股票代码 既可能在 A股票日行情表，也可能在 港股票日行情表 上！
-
-
-基金规模变动表 的 截止日期 月日 只有4种可能（季末），连同年一共12种取值
-基金份额持有人结构 的 个人/机构投资者持有的基金份额占总份额比例 是[0, 100]的数字
-
-基金可转债持仓明细 的 持仓日期 月日 有多种可能（即使限定了季报），因此必须用between查找
-
-A股公司行业划分表 行业划分标准=中信/申万行业分类 下，有部分一级行业名称（例如 汽车 计算机）会重叠
-
-基金基本信息 的 管理费率、托管费率 的取值形如 '1.2%', 经测试可以直接比较（如 管理费率 < '0.8%'  ）
-
-"""
+# TODO 事后检查： 有没有问题对不上模板、查询结果为空（原因可能为：没考虑港股、报告日期不在月末）
--- FinQwen/opt/experiment/sql_generator.py
+++ FinQwen/opt/experiment/sql_generator.py
@@ -22,6 +22,8 @@ from generate_sql_test_question import *
     """
 )
 def gen0(year=None, month=None):
+    months = ["03", "06", "09", "12"]
+
     return dict(
         year=year or choice(years),
         month=month or choice(months)
@@ -41,6 +43,8 @@ def gen0(year=None, month=None):
     """
 )
 def gen1(year=None, monthday=None):
+    monthdays = ["03-31", "06-30", "09-30", "12-31"]
+
     return dict(
         year=year or choice(years),
         monthday=monthday or choice(monthdays)
@@ -60,37 +64,45 @@ def gen1(year=None, monthday=None):
     """
 )
 def gen2(year=None, code=None, stock=None, compare=None):
-    stock = stock or choice(stocks)
+    stock = stock or choice_from_dict(stock_to_table)
     table = stock_to_table[stock]
-    compare = compare or choice(compares_v1)
+
+    compare_to_sign = {
+        "高于": ">",
+        "低于": "<"
+    }
+    compare = compare or choice_from_dict(compare_to_sign)
+
     return dict(
         year=year or choice(years),
-        code=code or random(table, "股票代码"),
+        code=code or choice_from_column(table, "股票代码"),
         stock=stock,
         compare=compare,
         table=table,
-        sign=compare_to_sign_v1[compare]
+        sign=compare_to_sign[compare]
     )
 
 
 @Decorator(
     cluster_label=3,
-    question_template="针对{year}年的{report}，有多少家基金的{unit}投资者持有份额占比不足{percent}%?",
+    question_template="针对{year}年的{report}，有多少家基金的{role}持有份额占比不足{percent}%?",
     sql_template="""
     SELECT COUNT(DISTINCT(基金代码)) AS 数量
     FROM 基金份额持有人结构
     WHERE 定期报告所属年度 = {year}
     AND 报告类型 = '{report}'
-    AND {unit}投资者持有的基金份额占总份额比例 < {percent}
+    AND {role}持有的基金份额占总份额比例 < {percent}
     LIMIT 1;
     """
 )
-def gen3a(year=None, report=None, unit=None, percent=None):
+def gen3a(year=None, report=None, role=None, percent=None):
+    roles = ["个人投资者", "机构投资者"]
     table = "基金份额持有人结构"
+
     return dict(
         year=year or choice(years),
-        report=report or random(table, "报告类型"),
-        unit=unit or choice(units),
+        report=report or choice_from_column(table, "报告类型"),
+        role=role or choice(roles),
         percent=percent or randint(1, 100)
     )
 
@@ -98,12 +110,12 @@ def gen3a(year=None, report=None, unit=None, percent=None):
 @Decorator(
     cluster_label=3,
     question_template="请帮我查询下，在{year}年{season}季报报告中，{code}基金的第{numzh}大重仓可转债同期还有多少只基金也进行了持仓？",
-    # 问题中包含”还有“，因此要扣除自己
+    # 问题是问“还有”，因此要扣除自己
     sql_template="""
     WITH t1 AS (
         SELECT *
         FROM 基金可转债持仓明细
-        WHERE 持仓日期 BETWEEN '{year}{start}' AND '{year}{end}'
+        WHERE 持仓日期 = '{year}{monthday}'
         AND 报告类型 = '季报'
     )
     SELECT COUNT(DISTINCT(基金代码)) AS 数量
@@ -120,17 +132,35 @@ def gen3a(year=None, report=None, unit=None, percent=None):
     """
 )
 def gen3b(year=None, season=None, code=None, numzh=None):
-    season = season or choice(seasons_v1)
-    start, end = season_to_start_end_v1[season]
-    numzh = numzh or choice(numzhs)
+    season_to_monthday = {
+        "Q1": "0331",
+        "Q2": "0630",
+        "Q3": "0930",
+        "Q4": "1231"
+    }
+    season = season or choice_from_dict(season_to_monthday)
+    monthday = season_to_monthday[season]
+
+    numzh_to_num = {
+        "一": 1,
+        "二": 2,
+        "三": 3,
+        "四": 4,
+        "五": 5,
+        "六": 6,
+        "七": 7,
+        "八": 8,
+        "九": 9
+    }
+    numzh = numzh or choice_from_dict(numzh_to_num)
     table = "基金可转债持仓明细"
+
     return dict(
         year=year or choice(years),
         season=season,
-        code=code or random(table, "基金代码"),
+        code=code or choice_from_column(table, "基金代码"),
         numzh=numzh,
-        start=start,
-        end=end,
+        monthday=monthday,
         num=numzh_to_num[numzh]
     )
 
@@ -157,10 +187,11 @@ def gen3b(year=None, season=None, code=None, numzh=None):
 )
 def gen4(name=None, date=None, report=None, standard=None):
     table = "基金可转债持仓明细"
+
     return dict(
-        name=name or random(table, "基金简称"),
-        date=date or random(table, "持仓日期"),
-        report=report or random(table, "报告类型"),
+        name=name or choice_from_column(table, "基金简称"),
+        date=date or choice_from_column(table, "持仓日期"),
+        report=report or choice_from_column(table, "报告类型"),
         standard=standard or choice(standards)
     )
 
@@ -180,10 +211,11 @@ def gen4(name=None, date=None, report=None, standard=None):
 )
 def gen5(date=None, report=None, name=None):
     table = "基金债券持仓明细"
+
     return dict(
-        date=date or random(table, "持仓日期"),
-        report=report or random(table, "报告类型"),
-        name=name or random(table, "基金简称")
+        date=date or choice_from_column(table, "持仓日期"),
+        report=report or choice_from_column(table, "报告类型"),
+        name=name or choice_from_column(table, "基金简称")
     )
 
 
@@ -200,16 +232,17 @@ def gen5(date=None, report=None, name=None):
 )
 def gen6(manager=None, category=None):
     table = "基金基本信息"
+
     return dict(
-        manager=manager or random(table, "管理人"),
-        category=category or random(table, "基金类型")
+        manager=manager or choice_from_column(table, "管理人"),
+        category=category or choice_from_column(table, "基金类型")
     )
 
 
 @Decorator(
     cluster_label=7,
-    # 问题没有明确是中信还是申万标准，有的只在一边，有的两边都有，不过不影响结果
     question_template="{date}日，一级行业为{industry1}的股票的{target}合计是多少？取整。",
+    # 问题没有明确是中信还是申万标准，有的一级行业只有一边有，有的两边都有，不过不影响结果
     sql_template="""
     SELECT CAST(SUM([{column}]) AS INTEGER) AS {target}合计
     FROM A股票日行情表
@@ -224,11 +257,16 @@ def gen6(manager=None, category=None):
     """
 )
 def gen7a(date=None, industry1=None, target=None):
-    target = target or choice(targets)
+    target_to_column = {
+        "成交量": "成交量(股)",
+        "成交金额": "成交金额(元)"
+    }
+    target = target or choice_from_dict(target_to_column)
     table = "A股公司行业划分表"
+
     return dict(
-        date=date or random(table, "交易日期"),
-        industry1=industry1 or random(table, "一级行业名称"),
+        date=date or choice_from_column(table, "交易日期"),
+        industry1=industry1 or choice_from_column(table, "一级行业名称"),
         target=target,
         column=target_to_column[target]
     )
@@ -246,15 +284,21 @@ def gen7a(date=None, industry1=None, target=None):
     """
 )
 def gen7b(date=None, compare=None, stock=None):
-    compare = compare or choice(compares_v2)
-    stock = stock or choice(stocks)
+    compare_to_sign = {
+        "正": ">",
+        "负": "<"
+    }
+    compare = compare or choice_from_dict(compare_to_sign)
+
+    stock = stock or choice_from_dict(stock_to_table)
     table = stock_to_table[stock]
+
     return dict(
-        date=date or random(table, "交易日"),
+        date=date or choice_from_column(table, "交易日"),
         compare=compare,
         stock=stock,
         table=table,
-        sign=compare_to_sign_v2[compare]
+        sign=compare_to_sign[compare]
     )
 
 
@@ -262,9 +306,9 @@ def gen7b(date=None, compare=None, stock=None):
     cluster_label=8,
     question_template="请帮我计算，在{date}，{standard}行业分类划分的一级行业为{industry1}行业中，涨跌幅最大股票的股票代码是？涨跌幅是多少？百分数保留两位小数。股票涨跌幅定义为：（收盘价 - 前一日收盘价 / 前一日收盘价）* 100%。",
     sql_template="""
-    SELECT 股票代码, ROUND(涨跌幅1 * 100, 2) || '%' AS 涨跌幅
+    SELECT 股票代码, ROUND(涨跌幅0 * 100, 2) || '%' AS 涨跌幅
     FROM (
-        SELECT 股票代码, ([收盘价(元)] / [昨收盘(元)] - 1) AS 涨跌幅1
+        SELECT 股票代码, ([收盘价(元)] / [昨收盘(元)] - 1) AS 涨跌幅0
         FROM A股票日行情表
         WHERE 股票代码 IN (
             SELECT 股票代码
@@ -274,17 +318,18 @@ def gen7b(date=None, compare=None, stock=None):
             AND 一级行业名称 = '{industry1}'
         )
         AND 交易日 = '{date}'
-        ORDER BY 涨跌幅1 DESC
+        ORDER BY 涨跌幅0 DESC
     )
     LIMIT 1;
     """
 )
 def gen8(date=None, standard=None, industry1=None):
     table = "A股公司行业划分表"
+
     return dict(
-        date=date or random(table, "交易日期"),
+        date=date or choice_from_column(table, "交易日期"),
         standard=standard or choice(standards),
-        industry1=industry1 or random(table, "一级行业名称")
+        industry1=industry1 or choice_from_column(table, "一级行业名称")
     )
 
 
@@ -301,14 +346,19 @@ def gen8(date=None, standard=None, industry1=None):
     """
 )
 def gen9(company=None, year=None, compare=None, percent=None):
-    compare = compare or choice(compares_v3)
+    compare_to_sign = {
+        "大": ">",
+        "小": "<"
+    }
+    compare = compare or choice_from_dict(compare_to_sign)
     table = "基金基本信息"
+
     return dict(
-        company=company or random(table, "管理人"),
+        company=company or choice_from_column(table, "管理人"),
         year=year or choice(years),
         compare=compare,
         percent=percent or randint(1, 9) / 10,
-        sign=compare_to_sign_v3[compare]
+        sign=compare_to_sign[compare]
     )
 
 
@@ -324,27 +374,32 @@ def gen9(company=None, year=None, compare=None, percent=None):
     """
 )
 def gen10(date=None, compare=None):
-    compare = compare or choice(compares_v4)
+    compare_to_sign = {
+        "上涨": ">",
+        "下跌": "<"
+    }
+    compare = compare or choice_from_dict(compare_to_sign)
     table = "港股票日行情表"
+
     return dict(
-        date=date or random(table, "交易日"),
+        date=date or choice_from_column(table, "交易日"),
         compare=compare,
-        sign=compare_to_sign_v4[compare]
+        sign=compare_to_sign[compare]
     )
 
 
 @Decorator(
     cluster_label=11,
     question_template="我想了解{name}基金,在{year}年{season}的季报第{rank}大重股。该持仓股票当个季度的涨跌幅?请四舍五入保留百分比到小数点两位。",
-    # 先把股票找出来，存到t1表中（只有一行数据），股票可能在A股表也可能在港股表，SQL貌似不支持动态选择表的操作，因此只能分别查询A股和港股表，
-    # 然后将答案union起来得到t4表（有且只有1个表有数据）。t4表包含目标股票在目标季度内的所有数据，分别找到最早的昨收价和最晚的收盘价（不一定是季度边缘），
+    # 先把股票找出来，存到t1表中（只有一条数据），股票可能在A股表也可能在港股表，SQL貌似不支持动态选择表的操作，因此只能分别查询A股和港股表，
+    # 然后将答案union起来得到t4表（有且只有1个表有数据）。t4表包含目标股票在目标季度内的所有数据，分别找到最早的昨收价和最晚的收盘价（不一定在季度第一天和最后一天），
     # 存到t5、t6表，最后就能算季度的涨跌幅了（join左右子表都只有1条数据，因此不用写on）
     sql_template="""
     WITH t1 AS (
         SELECT 股票代码
         FROM 基金股票持仓明细
         WHERE 基金简称 = '{name}'
-        AND 持仓日期 BETWEEN '{year}{start}' AND '{year}{end}'
+        AND 持仓日期 = '{year}{end}'
         AND 报告类型 = '季报'
         AND 第N大重仓股 = {rank}
         LIMIT 1
@@ -361,31 +416,109 @@ def gen10(date=None, compare=None):
         AND 交易日 BETWEEN '{year}{start}' AND '{year}{end}'
     ),
     t5 AS (
-        SELECT [昨收盘(元)]
+        SELECT [昨收盘(元)] AS 期初价
         FROM t4
         ORDER BY 交易日 ASC
         LIMIT 1
     ),
     t6 AS (
-        SELECT [收盘价(元)]
+        SELECT [收盘价(元)] AS 期末价
         FROM t4
         ORDER BY 交易日 DESC
         LIMIT 1
     )
-    SELECT ROUND((t6.[收盘价(元)] / t5.[昨收盘(元)] - 1) * 100, 2) || '%' AS 涨跌幅
+    SELECT ROUND((t6.期末价 / t5.期初价 - 1) * 100, 2) || '%' AS 涨跌幅
     FROM t5 JOIN t6
     LIMIT 1;
     """
 )
 def gen11(name=None, year=None, season=None, rank=None):
-    season = season or choice(seasons_v2)
-    start, end = season_to_start_end_v2[season]
+    season_to_start_end = {
+        "一季度": ("0101", "0331"),
+        "二季度": ("0401", "0630"),
+        "三季度": ("0701", "0930"),
+        "四季度": ("1001", "1231")
+    }
+    season = season or choice_from_dict(season_to_start_end)
+    start, end = season_to_start_end[season]
     table = "基金股票持仓明细"
+
     return dict(
-        name=name or random(table, "基金简称"),
+        name=name or choice_from_column(table, "基金简称"),
         year=year or choice(years),
         season=season,
         rank=rank or randint(1, 9),
         start=start,
         end=end
+    )
+
+
+@Decorator(
+    cluster_label=12,
+    question_template="我想知道{name}基金，在{year}年{report}中，前{rank}大重仓股中，有多少只股票在报告期内取得{compare}收益。",
+    # 问题11的加强版，从计算一个股票改为计算多个股票
+    # t5、t6还可以用 RANK() OVER (PARTITION BY 股票代码 ORDER BY 交易日 ASC) 的方式排序然后选择第1个，更通用
+    sql_template="""
+    WITH t1 AS (
+        SELECT 股票代码
+        FROM 基金股票持仓明细
+        WHERE 基金简称 = '{name}'
+        AND 持仓日期 = '{year}{end}'
+        AND 报告类型 = '年报(含半年报)'
+        AND 第N大重仓股 <= {rank}
+    ),
+    t4 AS (
+        SELECT *
+        FROM A股票日行情表 t2 JOIN t1
+        ON t2.股票代码 = t1.股票代码
+        AND 交易日 BETWEEN '{year}{start}' AND '{year}{end}'
+        UNION
+        SELECT *
+        FROM 港股票日行情表 t3 JOIN t1
+        ON t3.股票代码 = t1.股票代码
+        AND 交易日 BETWEEN '{year}{start}' AND '{year}{end}'
+    ),
+    t5 AS (
+        SELECT 股票代码, [昨收盘(元)] AS 期初价
+        FROM t4
+        GROUP BY 股票代码
+        HAVING 交易日 = MIN(交易日)
+    ),
+    t6 AS (
+        SELECT 股票代码, [收盘价(元)] AS 期末价
+        FROM t4
+        GROUP BY 股票代码
+        HAVING 交易日 = MAX(交易日)
+    )
+    SELECT COUNT(*) AS 数量
+    FROM t5 JOIN t6
+    ON t5.股票代码 = t6.股票代码
+    WHERE t6.期末价 {sign} t5.期初价
+    LIMIT 1;
+    """
+)
+def gen12(name=None, year=None, report=None, rank=None, compare=None):
+    report_to_start_end = {
+        "半年度报告": ("0101", "0630"),
+        "年度报告": ("0101", "1231")
+    }
+    report = report or choice_from_dict(report_to_start_end)
+    start, end = report_to_start_end[report]
+
+    compare_to_sign = {
+        "正": ">",
+        "负": "<"
+    }
+    compare = compare or choice_from_dict(compare_to_sign)
+    table = "基金股票持仓明细"
+
+    return dict(
+        name=name or choice_from_column(table, "基金简称"),
+        year=year or choice(years),
+        report=report,
+        rank=rank or randint(1, 10),
+        compare=compare,
+        start=start,
+        end=end,
+        sign=compare_to_sign[compare]
     )
